a9ef9b64fe2e4bc2886e3924a9593aeec83e8ceb
DreamNrr <wzh74371233@163.com>
2017-05-25 11:41:12 +0800

    导入Vitamio，创建自定义View

---
+++ app/src/main/java/com/example/wzh/mytest0525/view/VitamioVideoView.java
@@ -0,0 +1,30 @@
+package com.example.wzh.mytest0525.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+import io.vov.vitamio.widget.VideoView;
+
+/**
+ * Created by WZH on 2017/5/25.
+ */
+
+public class VitamioVideoView extends VideoView {
+    public VitamioVideoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(widthMeasureSpec,heightMeasureSpec);
+    }
+    public void setVideoSize(int width,int height){
+        ViewGroup.LayoutParams l = getLayoutParams();
+        l.width = width;
+        l.height = height;
+        setLayoutParams(l);
+    }
+
+}
