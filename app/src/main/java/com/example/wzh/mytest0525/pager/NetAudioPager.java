6c5fd3dffe426586cb0716ffc3485760b8b6e5eb
DreamNrr <wzh74371233@163.com>
2017-05-25 11:27:00 +0800

    主页面框架

---
+++ app/src/main/java/com/example/wzh/mytest0525/pager/NetAudioPager.java
@@ -0,0 +1,27 @@
+package com.example.wzh.mytest0525.pager;
+
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.wzh.mytest0525.fragment.BaseFragment;
+
+/**
+ * Created by WZH on 2017/5/25.
+ */
+
+public class NetAudioPager extends BaseFragment {
+    private TextView textView;
+    @Override
+    public View initView() {
+        textView = new TextView(context);
+        textView.setTextSize(30);
+        textView.setGravity(Gravity.CENTER);
+        return textView;
+    }
+    @Override
+    public void initData() {
+        super.initData();
+        textView.setText("网络音乐");
+    }
+}
