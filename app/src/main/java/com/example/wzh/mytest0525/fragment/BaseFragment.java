ebfdc340ffb29641a4aaf4a40fb6f729f51b300d
DreamNrr <wzh74371233@163.com>
2017-05-25 11:12:17 +0800

    创建基类

---
+++ app/src/main/java/com/example/wzh/mytest0525/fragment/BaseFragment.java
@@ -0,0 +1,41 @@
+package com.example.wzh.mytest0525.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by WZH on 2017/5/25.
+ */
+
+public abstract class BaseFragment extends Fragment {
+    private Context context;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context = getContext();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return initView();
+
+    }
+
+    public abstract View initView() ;
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initData();
+    }
+
+    public void initData() {}
+
+}
